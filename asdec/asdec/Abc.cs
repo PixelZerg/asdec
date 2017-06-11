5d412fcc49918d89f048ef556f3f4a0abbbba551
pixelzerg <abhinavbhandari4@gmail.com>
2017-06-11 22:11:55 +0100

    Abc Begin Impl
    Abc = Abc File Internal Model
    Build = build internal model
    
    PS: renamed folder name: agc

---
+++ asdec/asdec/Abc.cs
@@ -0,0 +1,57 @@
+﻿using asdec.Errors;
+using PygmentSharp.Core;
+using PygmentSharp.Core.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asdec
+{
+    public class Abc
+    {
+        public const bool DEBUG = true;
+
+        public FileInfo f = null;
+        public bool loaded = false;
+        internal List<Token> tokens = new List<Token>();
+
+        #region 
+        public Abc() { }
+        public Abc(string file)
+        {
+            f = new FileInfo(file);
+        }
+        public void Load(string file)
+        {
+            f = new FileInfo(file);
+            Load();
+        }
+
+        public void Load()
+        {
+            if (DEBUG) Console.WriteLine("Loading: " + f.Name);
+            Lex();
+            loaded = true;
+            if (DEBUG) Console.WriteLine("Loaded!");
+        }
+
+        public void Lex()
+        {
+            tokens = Pygmentize.File(f.FullName).WithLexer(new AVMLexer()).GetTokens().ToList();
+            if (DEBUG) Console.WriteLine("Lexed: {0} tokens", tokens.Count);
+        }
+        #endregion
+
+        public void Build()
+        {
+            if (!loaded)
+            {
+                if (f != null) Load();
+                else throw new AbcException("Abc file not loaded!");
+            }
+        }
+    }
+}
---
+++ asdec/asdec/Errors/AbcException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asdec.Errors
+{
+    public class AbcException : Exception
+    {
+        public AbcException()
+        {
+        }
+
+        public AbcException(string message) : base(message)
+        {
+        }
+
+        public AbcException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected AbcException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
