0f34bf2c2e07f91528d3c98f052532845637d273
pixelzerg <abhinavbhandari4@gmail.com>
2017-06-18 16:41:40 +0100

    idea

---
+++ asdec/asdec/ASModel/Dir.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asdec.ASModel
+{
+    public class Dir
+    {
+        public Dictionary<string, Dir> subdirs = new Dictionary<string, Dir>();
+    }
+}
--- asdec/asdec/Abc.cs
+++ asdec/asdec/Abc.cs
@@ -14,44 +14,20 @@ namespace asdec
     {
         public const bool DEBUG = true;
 
-        public DirectoryInfo d = null;
-        public bool loaded = false;
-        internal List<Token> tokens = new List<Token>();
+        public DirectoryInfo rootDir = null;
+
 
         #region 
         public Abc() { }
-        public Abc(string dir)
+        public Abc(string rootdir)
         {
-            d = new DirectoryInfo(dir);
-        }
-        public void Load(string dir)
-        {
-            d = new DirectoryInfo(dir);
-            Load();
-        }
-
-        public void Load()
-        {
-            if (DEBUG) Console.WriteLine("Loading: " + d.Name);
-            Lex();
-            loaded = true;
-            if (DEBUG) Console.WriteLine("Loaded!");
-        }
-
-        public void Lex()
-        {
-            tokens = Pygmentize.File(d.FullName).WithLexer(new AVMLexer()).GetTokens().ToList();
-            if (DEBUG) Console.WriteLine("Lexed: {0} tokens", tokens.Count);
+            rootDir = new DirectoryInfo(rootdir);
         }
         #endregion
 
         public void Build()
         {
-            if (!loaded)
-            {
-                if (d != null) Load();
-                else throw new AbcException("Abc file not loaded!");
-            }
+
         }
     }
 }
