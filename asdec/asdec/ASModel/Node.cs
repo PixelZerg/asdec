500c5968835f31097d84f167a7047f82276ac184
Abhinav Bhandari <abhinavbhandari4@gmail.com>
2017-06-19 16:22:58 +0000

    node structure deliberations

--- asdec/asdec/ASModel/Node.cs
+++ asdec/asdec/ASModel/Node.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +6,19 @@ using System.Threading.Tasks;
 
 namespace asdec.ASModel
 {
-    public class Node
+    public abstract class Node
     {
         public string Name = string.Empty;
         public List<Node> Children = new List<Node>();
-
-        public virtual Node Process()
-        {
-            //add # lookup here
+        
+        public virtual void Process();
+        
+        public void SkipWhitespace(){
+            //while true
+            //  look next token
+            //  if whitespace skip
+            //  else if start with # handle preprocessor
+            //  else return
         }
     }
 }
